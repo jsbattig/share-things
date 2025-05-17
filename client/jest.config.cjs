d80858dbf4ed5994a2b5169a6471536ecd48af67
Jose Sebastian Battig <jsbattig@gmail.com>
2025-05-16 22:31:43 -0500

    keep fixing shit

---
+++ client/jest.config.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true }],
+  },
+  extensionsToTreatAsEsm: ['.ts', '.tsx'],
+  coverageReporters: ['text', 'html'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/src/setupTests.ts',
+  ],
+  testMatch: [
+    '**/__tests__/**/*.test.(ts|tsx)'
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
